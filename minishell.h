cd753a20f54e711c568919f464b9aa2f6fe2eb39
Jolynn Collin <jcollin@pr-d5.kzn.21-school.ru>
2022-01-28 20:18:11 +0300

    add header

---
+++ minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+
+} t_
+
+#endif 
