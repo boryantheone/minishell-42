c20ae79701957e822f56f525df54ea3575c32899
Ddronel <ggg20211@yandex.ru>
2022-03-17 10:40:25 +0300

    correct parser

--- parser/ft_parser_utils.c
+++ parser/ft_parser_utils.c
@@ -12,6 +12,32 @@ void ft_skip_quotes(char **str)
 	*str = temp + i + 1;
 }
 
+void	ft_skip_redirect(char **str)
+{
+	char	*temp;
+	int		i;
+	
+	i = 0;
+	temp = ++(*str);
+	if (temp[i] && (temp[i] == '>' || temp[i] == '<'))
+		i++;
+	while (temp[i] && (temp[i] == ' ' || temp[i] == '\t'))
+		i++;
+	while (temp[i] && temp[i] != ' ')
+		i++;
+	*str = temp + i;
+}
+
+int ft_limiter(char c)
+{
+	if ((c >= 33 && c <= 47) || (c >= 58 && c <= 62) ||\
+		(c >= 91 && c <= 94) || (c >= 124 && c <= 126) \
+		|| c == 64 || c == '\0')
+		return (1);
+	else
+		return (0);
+}
+
 void ft_printfds(t_fds *elem)
 {
 	t_fds *tmp;
